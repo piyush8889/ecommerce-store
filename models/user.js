3b77cc71424e00f428e5ec1a394fd80d28e16307
piyush <singh345piyush@gmail.com>
2026-09-03 20:38:30 +0530

    add phone number to user model

--- models/user.js
+++ models/user.js
@@ -20,6 +20,11 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true
         },
+        phone: {
+            type: String,
+            required: true,
+            trim: true,
+        },
 
         role: {
             type: String,
