f81bc9aa40396bc5f4c6085c56a42aa9cdfba6b8
piyush <singh345piyush@gmail.com>
2026-09-06 20:55:42 +0530

    add JWT authentication middleware

---
+++ middlewares/user.middleware.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken")
+
+function authMiddleware(req,res,next) {
+    try {
+        const token = req.cookies.token
+
+        if(!token){
+            return res.status(401).json({
+                message: "Please login first"
+            })
+        }
+
+        const decoded = jwt.verify(token,process.env.JWT_SECRET)
+        req.user = decoded
+        next()
+
+    } catch (error) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid or expired token",
+        })
+    }
+}
+
+module.exports = {
+    authMiddleware,
+}
