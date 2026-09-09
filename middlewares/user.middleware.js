bb4a5c63979fffe9e5258ade79d3a1458d5d354c
piyush <singh345piyush@gmail.com>
2026-09-09 21:20:02 +0530

    add admin authorization middleware

--- middlewares/user.middleware.js
+++ middlewares/user.middleware.js
@@ -22,6 +22,16 @@ function authMiddleware(req,res,next) {
     }
 }
 
+// Admin middleware
+function adminMiddleware(req,res,next) {
+    if(!req.user.role !== "Admin"){
+        return res.status(403).json({
+            message: "Admin access required..."
+        })
+    }
+    next()
+}
+
 module.exports = {
     authMiddleware,
 }
