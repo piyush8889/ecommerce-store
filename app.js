9e7f30afe31803b9535acc9dd58e82302bf83a28
piyush <singh345piyush@gmail.com>
2026-09-17 16:09:27 +0530

    setup cart routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cookieParser = require("cookie-parser")
 
 const userRoute = require("./routes/user.routes")
 const productRoute = require("./routes/product.routes")
+const cartRoute = require("./routes/cart.routes")
 
 const app = express()
 
@@ -16,5 +17,6 @@ app.use(cookieParser())
 // route
 app.use("/api/v1/user",userRoute)
 app.use("/api/v1/product",productRoute)
+app.use("/api/v1/cart",cartRoute)
 
 module.exports = app
