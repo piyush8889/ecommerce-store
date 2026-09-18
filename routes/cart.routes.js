5ffcb62c784851b4adf78b6fabfdea9d96f9c12a
piyush <singh345piyush@gmail.com>
2026-09-18 23:49:07 +0530

    add auth middleware to cart routes

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const { authMiddleware } = require("../middlewares/user.middleware")
+
 
 const router = express.Router()
 
+router.use(authMiddleware)
 
 module.exports = router
